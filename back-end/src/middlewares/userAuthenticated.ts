0320eff7421feed27d4194dc1fd60fe328abf94d
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-08 01:30:06 -0300

    authenticated commit

---
+++ back-end/src/middlewares/userAuthenticated.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express"
+import { AppError } from "../shared/error/AppError";
+import authConfig from "../config/auth";
+import { verify } from "jsonwebtoken";
+
+interface ITokenPayload {
+    firstName: string;
+    lastName: string;
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+const userAuthenticated = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    //validando jwt
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("token not exist!", 401);
+    }
+
+    const [, token] = authHeader.split('');
+
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
+
+        const { sub, firstName, lastName } = decoded as ITokenPayload;
+
+        req.user = {
+            id: sub,
+            firstName,
+            lastName
+        }
+
+        return next()
+    } catch {
+        throw new AppError("token is invalid", 401);
+    }
+
+
+}
+
+export { userAuthenticated }
--- back-end/src/routes/pix.routes.ts
+++ back-end/src/routes/pix.routes.ts
@@ -4,5 +4,8 @@ import { Router } from "express";
 
 const pixRoutes = Router()
 
+// pixRoutes.post("/signin", userController.signin)
+// pixRoutes.post("/signup", userController.signup)
+// pixRoutes.get("/signup", userController.signup)
 
 export { pixRoutes }
