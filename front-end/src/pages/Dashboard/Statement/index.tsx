1b99af01f4eb6021c52e8c62566d13fc0cff2d32
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-08 18:09:26 -0300

    statement commit

---
+++ front-end/src/pages/Dashboard/Statement/index.tsx
@@ -0,0 +1,15 @@
+import {
+    Container,
+    StatementItemContainer,
+    StatementItemInfo,
+    StatementItemImage
+} from './styles';
+
+export const Statement = () => {
+
+    return (
+        <Container>
+
+        </Container>
+    );
+}
--- front-end/src/styles/theme/index.ts
+++ front-end/src/styles/theme/index.ts
@@ -8,6 +8,8 @@ export default {
         secondary: colors.SECONDARY,
         tertiary: colors.TERTIARY,
         background: colors.BACKGROUND,
-        backgroundLight: colors.BACKGROUND2
+        backgroundLight: colors.BACKGROUND2,
+        red: colors.RED,
+        green: colors.GREEN
     }
 }
