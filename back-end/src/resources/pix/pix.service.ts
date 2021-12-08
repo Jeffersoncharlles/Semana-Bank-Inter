3a20f9f6b2e357072ef7f2b3f31639d59722d72d
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-08 08:32:52 -0300

    controller pix pay commit

--- back-end/src/resources/pix/pix.service.ts
+++ back-end/src/resources/pix/pix.service.ts
@@ -1,7 +1,64 @@
+import { getRepository } from "typeorm";
+import { Pix } from "../../entity/Pix";
+import { User } from "../../entity/User";
+import { AppError } from "../../shared/error/AppError";
+import { decodeKey, encodeKey } from "../../utils/pix";
 
 
 
 class PixService {
+
+    async request(value: number, user: Partial<User>) {
+        const pixRepository = getRepository(Pix);
+        const userRepository = getRepository(User);
+
+        const currentUser = await userRepository.findOne({ where: { id: user.id } })
+
+        const requestData = {
+            requestingUser: currentUser,
+            value,
+            status: 'open',
+        }
+
+        const register = await pixRepository.save(requestData)
+
+        const key = encodeKey(user.id || '', value, register.id)
+
+        return key
+    }
+
+
+    async pay(key: string, user: Partial<User>) {
+        const keyDecoded = decodeKey(key);
+
+        if (keyDecoded.userId === user.id) {
+            throw new AppError("not possible to receive pix from the same user")
+        }
+
+        const pixRepository = getRepository(Pix);
+        const userRepository = getRepository(User);
+
+        //quem vai receber
+        const requestingUser = await userRepository.findOne({ where: { id: keyDecoded.userId } })
+
+        //quem vai pagar
+        const payingUser = await userRepository.findOne({ where: { id: user.id } })
+
+        if (payingUser?.wallet && payingUser.wallet < Number(keyDecoded.value)) {
+            throw new AppError("Não há saldo suficiente para fazer o pagamento", 401)
+        }
+        if (!requestingUser || !payingUser) {
+            throw new AppError("Não encontramos os clientes da transação, gere uma nova chave", 401)
+        }
+
+        requestingUser.wallet = Number(requestingUser?.wallet) + Number(keyDecoded.value);
+        await userRepository.save(requestingUser)
+
+        payingUser.wallet = Number(payingUser?.wallet) - Number(keyDecoded.value);
+        await userRepository.save(payingUser);
+
+        const pixTransaction = await pixRepository.findOne({ where: { id: keyDecoded.registerId, status: 'open' } })
+    }
 
 }
 
