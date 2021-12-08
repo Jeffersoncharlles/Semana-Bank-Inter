3fd4d9aee29aba66bba11302d85df4f3d750e0f0
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-08 08:45:27 -0300

    controller pix ok commit

--- back-end/src/resources/pix/pix.service.ts
+++ back-end/src/resources/pix/pix.service.ts
@@ -57,8 +57,58 @@ class PixService {
         payingUser.wallet = Number(payingUser?.wallet) - Number(keyDecoded.value);
         await userRepository.save(payingUser);
 
-        const pixTransaction = await pixRepository.findOne({ where: { id: keyDecoded.registerId, status: 'open' } })
+        const pixTransaction = await pixRepository.findOne({ where: { id: keyDecoded.registerId, status: 'open' } });
+
+        if (!pixTransaction) {
+            throw new AppError('Chave inválida para pagamento', 401)
+        }
+
+        pixTransaction.status = 'close';
+        pixTransaction.payingUser = payingUser
+
+        await pixRepository.save(pixTransaction)
+
+        return { mag: 'Pagamento efetuado com sucesso' }
     }
+
+    async transactions(user: Partial<User>) {
+        const pixRepository = getRepository(Pix);
+
+        const pixReceived = await pixRepository.find({ where: { requestingUser: user.id, status: 'close', relations: ['requestingUser'] } });
+
+        const pixPaying = await pixRepository.find({ where: { payingUser: user.id, status: 'close', relations: ['requestingUser'] } })
+
+        const received = pixReceived.map(transaction => ({
+            value: transaction.value,
+            user: {
+                firstName: transaction.payingUser.firstName,
+                lastName: transaction.payingUser.lastName
+            },
+            updated_at: transaction.updated_at,
+            type: 'received'
+        }))
+
+        const paying = pixPaying.map(transaction => ({
+            value: transaction.value,
+            user: {
+                firstName: transaction.requestingUser.firstName,
+                lastName: transaction.requestingUser.lastName
+            },
+            updated_at: transaction.updated_at,
+            type: 'paid'
+        }));
+
+        const allTransactions = received.concat(paying);
+
+        allTransactions.sort((a, b) => {
+            const dateA = new Date(a.updated_at).getTime();
+            const dateB = new Date(b.updated_at).getTime();
+            return dateA < dateB ? 1 : -1
+        });
+
+        return allTransactions;
+    }
+
 
 }
 
